61a84a076d7c36fdefcfbde6ccdc6a5d4ffb8566
wanderingslc <wanderingslc@gmail.com>
2023-02-08 09:48:33 -0700

    Second iteration through API, and adding views to display data.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,21 @@
 class HomeController < ApplicationController
   def index
+    @weather = 'waiting'
   end
 
   def api_call
-    binding.pry
-    @weather = JSON.parse(WeatherApiCall.new(params["city"]).call)
+
+
+    response = WeatherApiCall.new(params["search_term"], params['days']).call
+    if response[:status] == 200
+      @weather = response
+      respond_to do |format|
+        format.html {redirect_to root_path, notice: 'API call success'}
+        format.turbo_stream
+      end
+    else
+      redirect_to root_path, notice: 'Must include a term'
+    end
   end
+
 end
--- app/services/weather_api_call.rb
+++ app/services/weather_api_call.rb
@@ -1,16 +1,28 @@
 class WeatherApiCall
   require 'typhoeus'
+  require 'typhoeus/cache/rails'
+  Typhoeus::Config.cache = Typhoeus::Cache::Rails.new
+  def initialize(query, days)
 
-  def initialize(query)
     @query = query
+    @days = days
   end
   def call
+
     request = Typhoeus::Request.new(
-      'http://api.weatherapi.com/v1/current.json',
+      "http://api.weatherapi.com/v1/forecast.json?key=#{Rails.application.credentials.weather_key}&q=#{@query}&days=#{@days}&aqi=no&alerts=no",
       method: 'get',
-      params: {key: Rails.application.credentials.weather_key,
-               q: @query}
-    )
-    request.run.response_body
+      cache_ttl: 1800
+    ).run
+
+    p "cached #{request.cached?}"
+    parsed_body = JSON.parse(request.response_body)
+    {location: parsed_body['location'],
+     cached: request.cached?,
+     current: parsed_body['current'],
+     status: request.response_code,
+     days: parsed_body['forecast']['forecastday'].map {|d| d.fetch_values('date', 'day')},
+    }
   end
+
 end
